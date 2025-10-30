4f5c606bdcd4bca5c07dd21eef59383cba3bfb4a
Eric Hennigan <eric.hennigan@gmail.com>
2025-10-29 20:45:21 -0700

    Finish philips hue project
    - adjustments to get a better fit over the socket
     - assertions for the dimension of various parts
     - saving the final results 3mf project

--- philips-hue/hue_rocker.py
+++ philips-hue/hue_rocker.py
@@ -8,48 +8,65 @@ from ocp_vscode import *
 
 class HueRocker(Compound):
     def __init__(self):
-        height = 92.2
-        width = 35.2
-        depth = 10
+        height = 91.7
+        width = 34.7
+        depth = 5.6
+        recess = 2.5
 
         ALIGN_TOP = (Align.CENTER, Align.CENTER, Align.MAX)
 
         with BuildPart() as block:
-            Box(width, height, depth)
+            Box(width+2, height, depth)
             RigidJoint(
                 label="center",
                 joint_location=Location((0,0,depth/2)),
             )
+            bot_face = block.faces().sort_by(Axis.Z)[0]
 
             top_face = block.faces().sort_by(Axis.Z)[-1]
             with BuildPart(top_face, mode=Mode.SUBTRACT) as face_cutout:
-                Box(width-.01, height-.01, 3, align=ALIGN_TOP)
+                Box(width-.01, height-.01, recess, align=ALIGN_TOP)
             edges = face_cutout.faces().sort_by(Axis.Z)[0].edges()
             edges += face_cutout.edges().filter_by(Axis.Z)
-            fillet(edges, radius=2.99)
+            fillet(edges, radius=recess-.01)
 
             top_face = -face_cutout.faces().sort_by(Axis.Z)[0]
 
-            loc = Location((0, height/2-6, depth/2-3))
-            with BuildPart(loc, mode=Mode.SUBTRACT) as slot_cutout:
-                Box(26, 2.25, 1, align=ALIGN_TOP)
-            fillet(slot_cutout.edges().filter_by(Axis.Z), radius=0.75)
+            buffer = .2
+            # put the center of the slot 5.5 from the end of the object
+            with BuildPart(top_face, mode=Mode.SUBTRACT) as slot_cutout:
+                with Locations((0, height/2 - 5.5, 0)):
+                    b = Box(26, 1 + 2*buffer, 1, align=ALIGN_TOP)
+            fillet(slot_cutout.edges().filter_by(Axis.Z), radius=2*buffer)
 
+            # Decora rocker switch border is 66.5mm x 33mm
+            center_bar_height = 18
             with BuildPart(mode=Mode.SUBTRACT) as interior:
-                Box(width-2*3, height-2*10, depth)
+                h = 66.5/2 - center_bar_height/2
+                with Locations((0, center_bar_height/2)):
+                    b = Box(33, h, depth, align=(Align.CENTER, Align.MIN, Align.CENTER))
+                    fillet(b.edges().filter_by(Axis.Z), radius=2)
+                mirror(interior.part, about=Plane.XZ)
 
-            with BuildPart(top_face) as center_bar:
-                Box(width, 18, 3+.2+.2, align=ALIGN_TOP)
-            with BuildPart(top_face.offset(-.2), mode=Mode.SUBTRACT):
-                Cylinder(radius=7.6, height=4, align=ALIGN_TOP)
+            thick = .55
+            with BuildPart(top_face.offset(-thick), mode=Mode.SUBTRACT) as magnet:
+                c = Cylinder(radius=7.6, height=depth, align=ALIGN_TOP)
+                mag_face = c.faces().sort_by(Axis.Z)[-1]
+
+            mag_depth = abs(bot_face.center().Z - mag_face.center().Z)
+            print('Magnet depth', mag_depth)
+            assert mag_depth > 2.5, 'insufficent space for magnet'
+
 
         super().__init__(block.part.wrapped, joints=block.part.joints)
 
 
 if __name__ == '__main__':
-    #show_object([HueRocker()], reset_camera=Camera.KEEP)
-
     hr = HueRocker()
+
+    show_object([hr], reset_camera=Camera.KEEP)
+    sys.exit(0)
+
     with BuildPart() as part:
         Box(*hr.bounding_box().size)
 
--- philips-hue/wallplate.py
+++ philips-hue/wallplate.py
@@ -2,14 +2,10 @@
 from build123d import *
 from ocp_vscode import *
 from hue_rocker import HueRocker
-import copy
-import sys
 
 height = 5 * IN
 width = (1 + 13/16) * IN
-depth = 15
-recess = 5
-
+depth = 14
 
 # Notes for re-design
 #  - Gang (mm): 46.04 wide spacing
@@ -23,30 +19,29 @@ recess = 5
 #    - remote: 1.38 x 3.6 x 0.43 IN
 #    - with wall mount plate: 2.76 x 4.5 x 0.55 IN
 
+AlignCCT = (Align.CENTER, Align.CENTER, Align.MAX)
 
-gangs = [HueRocker()]
+gangs = [HueRocker()]*4
 
 with BuildPart() as plate:
-    thick = 4
+    thick = 2
     border_w = 0.5 * IN
     w = len(gangs) * width + 2 * border_w
 
     box = Box(w, height, depth)
     fillet(plate.edges().filter_by(Axis.Z), radius=3)
 
-    top_face = plate.faces().sort_by(Axis.Z)[0]
-    offset(amount=-thick, openings=top_face)
+    wall_face = plate.faces().sort_by(Axis.Z)[0]
+    room_face = plate.faces().sort_by(Axis.Z)[-1]
+    offset(amount=-thick, openings=wall_face)
 
-    bot_face = plate.faces().sort_by(Axis.Z)[-1]
-    chamfer(bot_face.edges(), length=2)
-
-    # Add bracing
-    Box(w, thick, depth-recess)
-    with GridLocations(x_spacing=width, x_count=len(gangs), y_spacing=0, y_count=1):
-        Box(thick, height, depth-recess)
-    with GridLocations(x_spacing=width, x_count=len(gangs), y_spacing=(3 + 13/16)*IN, y_count=2):
-        Cylinder(radius=4, height=depth-recess)
-    fillet(plate.edges().filter_by(Axis.Z), radius=2)
+    # Place the screw supports
+    plate_clearance = 2.5 # Distance between wall and screw supports
+    with GridLocations(x_spacing=width, x_count=len(gangs), y_spacing=width + 2*IN, y_count=2):
+        with Locations(wall_face.offset(-plate_clearance)):
+            c = Cylinder(radius=5, height=depth-plate_clearance,
+                    align=(Align.CENTER, Align.CENTER, Align.MAX))
+            wall_screws = c.faces().sort_by(Axis.Z)[0]
 
     # Place the gang switches
     with Locations(Plane.XY.offset(depth/2)):
@@ -56,16 +51,45 @@ with BuildPart() as plate:
             s = g.bounding_box().size
             loc = Location(loc.position + (0, 0, -s.Z/2))
             with BuildPart(loc, mode=Mode.SUBTRACT) as gang_cutout:
-                s.Z += 10
-                Box(*s)
+                b = Box(*s)
             with BuildPart(loc) as gang_switch:
                 add(g)
+            # hack to add a border that closes a visual gap to screws
+            # decora rocker switch border is 66.5mm x 33mm
+            with BuildPart(loc) as border:
+                z = loc.position.Z - wall_screws.center().Z - 0.2
+                bord = Box(33+1.3, 66.5+1.3, z, align=AlignCCT)
+                bord = offset(amount=-0.6, openings=bord.faces().filter_by(Plane.XY))
+                fillet(bord.edges().filter_by(Axis.Z), radius=0.25)
 
-    # Add the screw bores
-    with GridLocations(x_spacing=width, x_count=len(gangs), y_spacing=(3 + 13/16)*IN, y_count=2):
-        with Locations(bot_face):
+            # The rocker switch has a crossbar and needs clearance
+            f = b.faces().sort_by(Axis.Z)[0]
+            gang_clearance = abs(f.center().Z - wall_screws.center().Z)
+            print('Gang clearance', gang_clearance)
+            assert gang_clearance >= 5.89, "Insufficent clearance for gang"
+
+    # Bore the screws
+    with GridLocations(x_spacing=width, x_count=len(gangs), y_spacing=width+2*IN, y_count=2):
+        with Locations((0, 0, wall_screws.center().Z + 3)):
             CounterSinkHole(radius=2, counter_sink_radius=4)
-            
+
+    # fillet where possible
+    for num, edge in enumerate(plate.edges().filter_by(Axis.Z)):
+        try:
+            fillet([edge], radius=2)
+        except Exception:
+            continue
+    chamfer(room_face.edges(), length=2)
+    chamfer(plate.faces().sort_by(Axis.Z)[0].edges(), length=0.2)
+
+    # For shim adjustment
+    #with Locations(room_face.offset(-depth+1)):
+    #    Box(w, height, depth, align=(Align.CENTER, Align.CENTER, Align.MIN), mode=Mode.SUBTRACT)
+
+
+
+import os
+os.chdir(os.path.dirname(__file__))
 
 show([plate], reset_camera=Camera.KEEP)
 export_stl(plate.part, 'wallplate.stl')
